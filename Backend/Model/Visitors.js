80a072862ece986392417d17eee8179835aa25e5
krishna23tiwari <krishna23tiwari.kt@gmail.com>
2025-07-26 18:45:07 +0530

    model updated

--- Backend/Model/Visitors.js
+++ Backend/Model/Visitors.js
@@ -1,18 +1,46 @@
+// const mongoose = require("mongoose");
+
+// const VisitorSchema = new mongoose.Schema({
+//   ip: { 
+//     type: String, 
+//     required: true, 
+  
+// },
+
+// //   date: 
+// //   { type: Date, 
+// //     default: Date.now 
+// // }
+
+//  date: { type: String, required: true }
+// },{timestamps: true, versionKey : false});
+
+// module.exports = mongoose.model("Visitor", VisitorSchema);
+
+
+// const mongoose = require("mongoose");
+
+// const VisitorSchema = new mongoose.Schema({
+//   ip: { type: String, required: true },
+//   date: { type: String, required: true },
+//   country: String,
+//   region: String,
+//   city: String,
+//   isp: String,
+//   lat: Number,
+//   lon: Number,
+// }, { timestamps: true, versionKey: false });
+
+// module.exports = mongoose.model("Visitor", VisitorSchema);
+
+
 const mongoose = require("mongoose");
 
-const VisitorSchema = new mongoose.Schema({
-  ip: { 
-    type: String, 
-    required: true, 
-  
-},
+const visitorSchema = new mongoose.Schema({
+  ip: String,
+  date: String,
+  location: String,
+},{timestamps: true, versionKey: false});
 
-//   date: 
-//   { type: Date, 
-//     default: Date.now 
-// }
+module.exports = mongoose.model("Visitor", visitorSchema);
 
- date: { type: String, required: true }
-},{timestamps: true, versionKey : false});
-
-module.exports = mongoose.model("Visitor", VisitorSchema);
