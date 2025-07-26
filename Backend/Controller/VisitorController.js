90d2edc4320809273f1f678daee86bc5141382ca
krishna23tiwari <krishna23tiwari.kt@gmail.com>
2025-07-26 18:23:22 +0530

    new setup

--- Backend/Controller/VisitorController.js
+++ Backend/Controller/VisitorController.js
@@ -1,81 +1,119 @@
-const requestIp = require("request-ip");
-const Visitor = require("../Model/Visitors");
-const Counter = require("../Model/Counter");
-const moment = require("moment");
-const { getLocationFromIP } = require("../Utility/ipLocation");
+// const requestIp = require("request-ip");
+// const Visitor = require("../Model/Visitors");
+// const Counter = require("../Model/Counter");
+// const moment = require("moment");
+// const { getLocationFromIP } = require("../Utility/ipLocation");
 
-// exports.trackVisitor = async (req, res) => {
-//   // const ip = req.ip || req.connection.remoteAddress;
+// // exports.trackVisitor = async (req, res) => {
+// //   // const ip = req.ip || req.connection.remoteAddress;
 
-//   const ip =
-//   req.headers["x-forwarded-for"]?.split(",")[0] || 
-//   req.connection.remoteAddress || 
-//   req.ip;
+// //   const ip =
+// //   req.headers["x-forwarded-for"]?.split(",")[0] || 
+// //   req.connection.remoteAddress || 
+// //   req.ip;
 
-//   const today = moment().format("YYYY-MM-DD");
+// //   const today = moment().format("YYYY-MM-DD");
 
-//   try {
-//     const existingVisitor = await Visitor.findOne({ ip });
+// //   try {
+// //     const existingVisitor = await Visitor.findOne({ ip });
 
-//     let counter = await Counter.findOne({ date: today });
+// //     let counter = await Counter.findOne({ date: today });
 
-//     if (!counter) {
+// //     if (!counter) {
       
-//       await Visitor.deleteMany({});
-//       counter = await Counter.create({
-//         date: today,
-//         todayCount: 1,
-//         totalCount: (await Counter.findOne().sort({ _id: -1 }))?.totalCount + 1 || 1
-//       });
-//       await Visitor.create({ ip });
-//     } else if (!existingVisitor) {
+// //       await Visitor.deleteMany({});
+// //       counter = await Counter.create({
+// //         date: today,
+// //         todayCount: 1,
+// //         totalCount: (await Counter.findOne().sort({ _id: -1 }))?.totalCount + 1 || 1
+// //       });
+// //       await Visitor.create({ ip });
+// //     } else if (!existingVisitor) {
      
-//       await Visitor.create({ ip });
-//       counter.todayCount += 1;
-//       counter.totalCount += 1;
-//       await counter.save();
-//     }
+// //       await Visitor.create({ ip });
+// //       counter.todayCount += 1;
+// //       counter.totalCount += 1;
+// //       await counter.save();
+// //     }
 
-//     res.status(200).json({ todayCount: counter.todayCount, totalCount: counter.totalCount });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: "Server error" });
-//   }
-// };
+// //     res.status(200).json({ todayCount: counter.todayCount, totalCount: counter.totalCount });
+// //   } catch (err) {
+// //     console.error(err);
+// //     res.status(500).json({ message: "Server error" });
+// //   }
+// // };
 
-// exports.trackVisitor = async (req, res) => {
-//   const ip = requestIp.getClientIp(req); // <-- ✅ reliable IP extraction
-//   console.log("Detected IP:", ip);
+// // exports.trackVisitor = async (req, res) => {
+// //   const ip = requestIp.getClientIp(req); // <-- ✅ reliable IP extraction
+// //   console.log("Detected IP:", ip);
 
-//   const today = moment().format("YYYY-MM-DD");
+// //   const today = moment().format("YYYY-MM-DD");
 
-//   try {
-//     const existingVisitor = await Visitor.findOne({ ip });
+// //   try {
+// //     const existingVisitor = await Visitor.findOne({ ip });
 
-//     let counter = await Counter.findOne({ date: today });
+// //     let counter = await Counter.findOne({ date: today });
 
-//     if (!counter) {
-//       await Visitor.deleteMany({});
-//       counter = await Counter.create({
-//         date: today,
-//         todayCount: 1,
-//         totalCount: (await Counter.findOne().sort({ _id: -1 }))?.totalCount + 1 || 1
-//       });
-//       await Visitor.create({ ip });
-//     } else if (!existingVisitor) {
-//       await Visitor.create({ ip });
-//       counter.todayCount += 1;
-//       counter.totalCount += 1;
-//       await counter.save();
-//     }
+// //     if (!counter) {
+// //       await Visitor.deleteMany({});
+// //       counter = await Counter.create({
+// //         date: today,
+// //         todayCount: 1,
+// //         totalCount: (await Counter.findOne().sort({ _id: -1 }))?.totalCount + 1 || 1
+// //       });
+// //       await Visitor.create({ ip });
+// //     } else if (!existingVisitor) {
+// //       await Visitor.create({ ip });
+// //       counter.todayCount += 1;
+// //       counter.totalCount += 1;
+// //       await counter.save();
+// //     }
 
-//     res.status(200).json({ todayCount: counter.todayCount, totalCount: counter.totalCount });
-//   } catch (err) {
-//     console.error("TrackVisitor error:", err);
-//     res.status(500).json({ message: "Server error" });
-//   }
-// };
+// //     res.status(200).json({ todayCount: counter.todayCount, totalCount: counter.totalCount });
+// //   } catch (err) {
+// //     console.error("TrackVisitor error:", err);
+// //     res.status(500).json({ message: "Server error" });
+// //   }
+// // };
 
+
+// // exports.trackVisitor = async (req, res) => {
+// //   const ip = requestIp.getClientIp(req);
+// //   const today = moment().format("YYYY-MM-DD");
+
+// //   try {
+// //     const existingVisitor = await Visitor.findOne({ ip, date: today });
+// //     let counter = await Counter.findOne({ date: today });
+
+// //     if (!counter) {
+// //       const lastCounter = await Counter.findOne().sort({ _id: -1 });
+// //       const totalCount = lastCounter?.totalCount || 0;
+
+// //       counter = await Counter.create({
+// //         date: today,
+// //         todayCount: 1,
+// //         totalCount: totalCount + 1,
+// //       });
+
+// //       await Visitor.create({ ip, date: today });
+// //     } else if (!existingVisitor) {
+// //       try {
+// //         await Visitor.create({ ip, date: today });
+// //         counter.todayCount += 1;
+// //         counter.totalCount += 1;
+// //         await counter.save();
+// //       } catch (e) {
+// //         if (e.code !== 11000) throw e;
+// //       }
+// //     }
+
+// //     res.status(200).json({ todayCount: counter.todayCount, totalCount: counter.totalCount });
+// //   } catch (err) {
+// //     console.error("❌ TrackVisitor error:", err.message);
+// //     console.error(err.stack);
+// //     res.status(500).json({ message: "Server error", error: err.message });
+// //   }
+// // };
 
 // exports.trackVisitor = async (req, res) => {
 //   const ip = requestIp.getClientIp(req);
@@ -86,64 +124,80 @@ const { getLocationFromIP } = require("../Utility/ipLocation");
 //     let counter = await Counter.findOne({ date: today });
 
 //     if (!counter) {
-//       const lastCounter = await Counter.findOne().sort({ _id: -1 });
-//       const totalCount = lastCounter?.totalCount || 0;
-
+//       await Visitor.deleteMany({}); // reset daily visitors
 //       counter = await Counter.create({
 //         date: today,
 //         todayCount: 1,
-//         totalCount: totalCount + 1,
+//         totalCount: (await Counter.findOne().sort({ _id: -1 }))?.totalCount + 1 || 1,
 //       });
 
-//       await Visitor.create({ ip, date: today });
+//       const location = await getLocationFromIP(ip);
+//       await Visitor.create({ ip, date: today, location });
 //     } else if (!existingVisitor) {
-//       try {
-//         await Visitor.create({ ip, date: today });
-//         counter.todayCount += 1;
-//         counter.totalCount += 1;
-//         await counter.save();
-//       } catch (e) {
-//         if (e.code !== 11000) throw e;
-//       }
+//       const location = await getLocationFromIP(ip);
+//       await Visitor.create({ ip, date: today, location });
+//       counter.todayCount += 1;
+//       counter.totalCount += 1;
+//       await counter.save();
 //     }
 
 //     res.status(200).json({ todayCount: counter.todayCount, totalCount: counter.totalCount });
-//   } catch (err) {
-//     console.error("❌ TrackVisitor error:", err.message);
-//     console.error(err.stack);
-//     res.status(500).json({ message: "Server error", error: err.message });
+//   } catch (error) {
+//     console.error("TrackVisitor error:", error);
+//     res.status(500).json({ message: "Server error" });
 //   }
 // };
 
+const Visitor = require("../Model/Visitors");
+const Counter = require("../Model/Counter");
+const moment = require("moment");
+const getLocationFromIP = require("../Utility/ipLocation");
+
 exports.trackVisitor = async (req, res) => {
-  const ip = requestIp.getClientIp(req);
+  const ip =
+    req.headers["x-forwarded-for"]?.split(",")[0] ||
+    req.connection.remoteAddress ||
+    req.socket?.remoteAddress ||
+    req.ip ||
+    "Unknown";
+
   const today = moment().format("YYYY-MM-DD");
 
   try {
     const existingVisitor = await Visitor.findOne({ ip, date: today });
-    let counter = await Counter.findOne({ date: today });
 
-    if (!counter) {
-      await Visitor.deleteMany({}); // reset daily visitors
-      counter = await Counter.create({
-        date: today,
-        todayCount: 1,
-        totalCount: (await Counter.findOne().sort({ _id: -1 }))?.totalCount + 1 || 1,
-      });
+    if (!existingVisitor) {
+      const location = await getLocationFromIP(ip); // <-- may be failing here
+      await Visitor.create({ ip, date: today, location });
 
-      const location = await getLocationFromIP(ip);
-      await Visitor.create({ ip, date: today, location });
-    } else if (!existingVisitor) {
-      const location = await getLocationFromIP(ip);
-      await Visitor.create({ ip, date: today, location });
-      counter.todayCount += 1;
-      counter.totalCount += 1;
-      await counter.save();
+      let counter = await Counter.findOne({ date: today });
+
+      if (!counter) {
+        const total = await Counter.findOne().sort({ totalCount: -1 });
+        const totalCount = total ? total.totalCount : 0;
+
+        await Counter.create({
+          date: today,
+          todayCount: 1,
+          totalCount: totalCount + 1,
+        });
+      } else {
+        counter.todayCount += 1;
+        counter.totalCount += 1;
+        await counter.save();
+      }
     }
 
-    res.status(200).json({ todayCount: counter.todayCount, totalCount: counter.totalCount });
+    const todayCounter = await Counter.findOne({ date: today });
+    const totalCounter = await Counter.find().sort({ totalCount: -1 }).limit(1);
+
+    return res.status(200).json({
+      todayCount: todayCounter?.todayCount || 0,
+      totalCount: totalCounter[0]?.totalCount || 0,
+    });
   } catch (error) {
-    console.error("TrackVisitor error:", error);
-    res.status(500).json({ message: "Server error" });
+    console.error("Visitor tracking error:", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
+
--- Backend/Model/Visitors.js
+++ Backend/Model/Visitors.js
@@ -18,17 +18,29 @@
 // module.exports = mongoose.model("Visitor", VisitorSchema);
 
 
+// const mongoose = require("mongoose");
+
+// const VisitorSchema = new mongoose.Schema({
+//   ip: { type: String, required: true },
+//   date: { type: String, required: true },
+//   country: String,
+//   region: String,
+//   city: String,
+//   isp: String,
+//   lat: Number,
+//   lon: Number,
+// }, { timestamps: true, versionKey: false });
+
+// module.exports = mongoose.model("Visitor", VisitorSchema);
+
+
 const mongoose = require("mongoose");
 
-const VisitorSchema = new mongoose.Schema({
-  ip: { type: String, required: true },
-  date: { type: String, required: true },
-  country: String,
-  region: String,
-  city: String,
-  isp: String,
-  lat: Number,
-  lon: Number,
-}, { timestamps: true, versionKey: false });
+const visitorSchema = new mongoose.Schema({
+  ip: String,
+  date: String,
+  location: String,
+});
 
-module.exports = mongoose.model("Visitor", VisitorSchema);
+module.exports = mongoose.model("Visitor", visitorSchema);
+
