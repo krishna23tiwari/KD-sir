5c00378e9537b75ea5eadd26ed3e674a2c6c9228
krishna23tiwari <krishna23tiwari.kt@gmail.com>
2025-08-02 13:36:56 +0530

    data

--- Frontend/src/components/BaseUrl.js
+++ Frontend/src/components/BaseUrl.js
@@ -1,4 +1,5 @@
-const baseurl = "http://localhost:5678/"
+// const baseurl = "http://localhost:5678/"
 // const baseurl = "https://updated-file-portfolio-1.onrender.com/"
+const baseurl = "https://updated-file-portfolio.onrender.com/"
 
 export default baseurl
