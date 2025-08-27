ce3c3cb0ef5ded8f3da474deeab8b6f67a87f400
krishna23tiwari <krishna23tiwari.kt@gmail.com>
2025-08-27 17:41:04 +0530

    url change

--- Frontend/src/components/BaseUrl.js
+++ Frontend/src/components/BaseUrl.js
@@ -1,6 +1,6 @@
-const baseurl = "http://localhost:5678/"
+// const baseurl = "http://localhost:5678/"
 // const baseurl = "https://updated-file-portfolio-1.onrender.com/"
 // const baseurl = "https://updated-file-portfolio.onrender.com/"
-
+const baseurl = "https://kd-7mh6.onrender.com"
 
 export default baseurl  
